698a05e59712f89d1d4a3676ac2470fc6688a075
Ronaldo Drecksler <estudansator@gmail.com>
2024-05-27 13:24:48 -0300

    FINALDOMENTE FILLPOLY FUNCIONOU

--- Cena3D.py
+++ Cena3D.py
@@ -19,20 +19,27 @@ class Cena3D:
         self.cor_buffer = np.full((self.height, self.width, 3), [
             174, 174, 174], dtype=np.uint8)
         for obj in self.objetos:
-            obj.rotacaoX(4)
-
-        self.camera_pos = [-20, 0, 0]
-        self.camera_lookat = [0, 0, 0]
+            obj.rotacaoX(36)
+        self.axis = False
+        self.camera_pos = [500, 600, 1000]
+        self.camera_lookat = [600, 0, 0]
 
         pg.font.init()
         self.font = pg.font.SysFont(None, 36)
 
-        self.__cores_faces = [[(random.uniform(0, 1), random.uniform(
-            0, 1), random.uniform(0, 1)) for _ in obj.get_faces()] for obj in self.objetos]
+        # self.__cores_faces = [[(random.uniform(0, 1), random.uniform(
+        #     0, 1), random.uniform(0, 1)) for _ in obj.get_faces()] for obj in self.objetos]
 
     def create_objetos(self):
         self.camera = Camera(self.camera_pos, self.camera_lookat, (0, 1, 0))
-        self.projetion = Projetion().projetion_matrix(300)
+        self.projetion = Projetion().projetion_matrix(500)
+        if self.axis:
+            self.projetion = np.array([
+                [1,0,0,0],
+                [0,1,0,0],
+                [0,0,0,0],
+                [0,0,0,0]
+            ])
         self.to_screen = Projetion().to_screen(-self.width//2, self.width//2, -
                                                self.height//2, self.height//2, 0, self.width, 0, self.height)
 
@@ -136,21 +143,21 @@ class Cena3D:
         #             self.screen.set_at((x, y), color)
         #             startZ += deltaZ
 
-    def draw_vertices(self, points):
+    def draw_vertices(self, points = []):
         for obj_idx, o in enumerate(self.objetos):
             # faces = o.get_faces_visible(self.camera_pos)
             # faces = o.get_faces()
+            # vertices = o.get_vertices()
+            # ones_column = np.ones((vertices.shape[0], 1))
+            # new_array = np.hstack((vertices, ones_column))
 
-            # vertices = self.create_objetos() @ o.get_vertices().T
+            # vertices = self.create_objetos() @ new_array.T[:4]
             # vertices[[0, 1]] /= vertices[-1]
             # # vertices[[0, 1]] = np.round(vertices[[0, 1]], 1)
             # vertices = vertices.T
-            # print(np.round(np.array(vertices), 1))
-            # print(np.round(o.get_vertices(), 1))
-            # print(np.array([f.vertices for f in faces ]))
 
             # for face_idx, face in enumerate(faces):
-            #     points = vertices[face.vertices].T[:2].T.astype(int)
+                # points = vertices[face.vertices].T[:2].T.astype(int)
 
                 for p in points:
                     pg.draw.circle(self.screen, (255, 255, 255), p, 3)
@@ -205,6 +212,94 @@ class Cena3D:
                 'taxaZ': (v0['z'] - v2['z']) / (v0['y'] - v2['y']),
             }
         ]
+        swaped = False
+        lastIniX, lastFimX = arestas[0]['ini']['x'], arestas[0]['ini']['x']
+        for y in range(round(v0['y']), round(v1['y'])):
+            lastIniX += arestas[0]['taxa']
+            lastFimX += arestas[2]['taxa']
+            intervalo = [lastIniX, lastFimX]
+
+            if intervalo[1] < intervalo[0]:
+                swaped = True
+                intervalo[0], intervalo[1] = intervalo[1], intervalo[0]
+
+            intervalo[0] = round(intervalo[0])
+            intervalo[1] = round(intervalo[1])
+
+            z = v0['z']
+            varX = intervalo[1] - intervalo[0] + 1e-16
+            deltaZ = (v1['z'] - v0['z']) / varX
+            for j in range(intervalo[0], intervalo[1]):
+                self.screen.set_at((j, y), color)
+                z += deltaZ
+
+        swaped = False
+        lastIniX = arestas[1]['ini']['x']
+
+        for y in range(round(v1['y']), round(v2['y'])):
+            lastIniX += arestas[1]['taxa']
+            lastFimX += arestas[2]['taxa']
+            intervalo = [lastIniX, lastFimX]
+
+            if intervalo[1] < intervalo[0]:
+                intervalo[0], intervalo[1] = intervalo[1], intervalo[0]
+                swaped = True
+
+            intervalo[0] = round(intervalo[0])
+            intervalo[1] = round(intervalo[1])
+
+            z = v1['z']
+            varX = intervalo[1] - intervalo[0]
+            deltaZ = (v2['z'] - v1['z']) / varX
+
+            for j in range(intervalo[0], intervalo[1]):
+                self.screen.set_at((j, y), color)
+                z += deltaZ
+
+    def fillpoli2(self, face, all_vertices, color):
+        vertices = all_vertices[face.vertices]
+        # self.poly.pontos.sort(key=lambda p: p['y'])
+        # v0, v1, v2 = self.poly.pontos[0], self.poly.pontos[1], self.poly.pontos[2]
+
+        vertices = sorted(vertices, key=lambda v: v[1])
+        # vertices = selected_vertices
+
+        # (x0, y0), z0 = map(int, vertices[0][:2]), float(vertices[0][2])
+        # (x1, y1), z1 = map(int, vertices[1][:2]), float(vertices[1][2])
+        # (x2, y2), z2 = map(int, vertices[2][:2]), float(vertices[2][2])
+        v0 = {
+            'x': vertices[0][0],
+            'y': vertices[0][1],
+            'z': vertices[0][2],
+        }
+        v1 = {
+            'x': vertices[1][0],
+            'y': vertices[1][1],
+            'z': vertices[1][2],
+        }
+        v2 = {
+            'x': vertices[2][0],
+            'y': vertices[2][1],
+            'z': vertices[2][2],
+        }
+
+        arestas = [
+            {
+                'ini': v0,
+                'fim': v1,
+                'taxa': (v1['x'] - v0['x']) / (v1['y'] - v0['y']),
+            },
+            {
+                'ini': v1,
+                'fim': v2,
+                'taxa': (v2['x'] - v1['x']) / (v2['y'] - v1['y']),
+            },
+            {
+                'ini': v2,
+                'fim': v0,
+                'taxa': (v0['x'] - v2['x']) / (v0['y'] - v2['y']),
+            }
+        ]
 
         lastIniX, lastFimX = arestas[0]['ini']['x'], arestas[0]['ini']['x']
         for y in range(round(v0['y']), round(v1['y'])):
@@ -251,36 +346,36 @@ class Cena3D:
             vertices = o.get_vertices()
             ones_column = np.ones((vertices.shape[0], 1))
             new_array = np.hstack((vertices, ones_column))
-            
+
             vertices = self.create_objetos() @ new_array.T[:4]
+            print(vertices)
             vertices[[0, 1]] /= vertices[-1]
             # vertices[[0, 1]] = np.round(vertices[[0, 1]], 1)
             vertices = vertices.T
 
-
-            # vertices = [(100, 100, 23), (400, 300, -45), (200, 500, 23)]
-            # bvertices = [(100, 100), (400, 300), (200, 500)]
-            # z_values = [0.5, 0.3, 0.7]
-
-            # Rasterizando o triângulo e atualizando os buffers
-            # self.rasterize_triangle(
-            #     bvertices, z_values, colors, self.z_buffer, cor_buffer)
-
+            # vertices = np.array([
+            #     [100,100, 32],
+            #     [100,440,24],
+            #     [200,200,23],
+            #     [440,140,32]
+            # ])
+            # faces = []
+            # faces.append(Face(vertices, [0,3,2]))
+            # faces.append(Face(vertices, [0,1,2]))
+            # faces.append(Face(vertices, [1,2,3])) # Esse aqui apresenta erro 
             for face_idx, face in enumerate(faces):
-                # vs = vertices[face.vertices][:2]
-                # points = vertices[face.vertices].T[:2].T
-                self.fillpoli(face, vertices,
-                          (200//(2+1), 100//(3+1), 0))
+                self.fillpoli(face, vertices, (100//(face_idx+1), 200//(face_idx+1),  face_idx))
+                # self.fillpoly(face, vertices, (100//(face_idx+1), 200//(face_idx+1),  face_idx))
                 # self.fillpoli(face, vertices, ())
-            # self.draw_vertices(bvertices)
-            # pg.draw.polygon(self.screen, (255, 0, 255), bvertices)
-            # pg.surfarray.blit_array(self.screen, cores)
 
+            self.draw_vertices(vertices.T[:2].T)
+            pg.display.flip()
 
     def draw_mouse_coords(self, screen, x, y):
         coord_text = f"X: {x}, Y: {y}"
-        text_surf = self.font.render(coord_text, True, (0, 0, 0))
-        screen.blit(text_surf, (10, 70))
+        # text_surf = self.font.render(coord_text, True, (0, 255, 255))
+        # print(coord_text)
+        # screen.blit(text_surf, (10, 70))
 
     def draw_cam_coords(self, screen, x, y, z):
         coord_text = f"X: {x}, Y: {y}, Z: {z}"
@@ -292,60 +387,10 @@ class Cena3D:
         text_surf = self.font.render(coord_text, True, (100, 100, 200))
         screen.blit(text_surf, (10, 105))
 
-    def rasterize_triangle(self, vertices, z_values, z_buffer=0, colors=0, color_buffer=0):
-        # Ordena os vértices verticalmente
-        sorted_vertices = sorted(vertices, key=lambda vertex: vertex[1])
-    # Obtém os vértices ordenados
-        v0, v1, v2 = sorted_vertices
-        # Calcula os gradientes de profundidade (z) e cor
-        dz_dx = [(v2[1] - v1[1]) * z_values[0] + (v1[0] - v2[0]) * z_values[2],
-                 (v0[1] - v2[1]) * z_values[1] + (v2[0] - v0[0]) * z_values[2],
-                 (v1[1] - v0[1]) * z_values[1] + (v0[0] - v1[0]) * z_values[0]]
-        dc_dx = [(v2[1] - v1[1]) * colors[0] + (v1[0] - v2[0]) * colors[2],
-                 (v0[1] - v2[1]) * colors[1] + (v2[0] - v0[0]) * colors[2],
-                 (v1[1] - v0[1]) * colors[1] + (v0[0] - v1[0]) * colors[0]]
-
-        # Determina a variação vertical de cada aresta
-        dy_01 = v1[1] - v0[1]
-        dy_12 = v2[1] - v1[1]
-        dy_20 = v0[1] - v2[1]
-
-        # Itera por cada scanline (linha de varredura)
-        for y in range(int(v0[1]), int(v2[1]) + 1):
-            # Determina a proporção da varredura entre os vértices
-            alpha = (y - v0[1]) / dy_20 if dy_20 != 0 else 0
-            beta = (y - v1[1]) / dy_01 if dy_01 != 0 else 0
-            gamma = (y - v2[1]) / dy_12 if dy_12 != 0 else 0
-
-            # Calcula os valores de profundidade (z) e cor para a linha de varredura
-            z_start = v0[0] + alpha * (v2[0] - v0[0])
-            z_end = v1[0] + beta * (v2[0] - v1[0])
-            dz_dx_scan = z_start + (z_end - z_start) * (np.arange(v0[0], v1[0] + 1) - v0[0]) / (
-                v1[0] - v0[0]) if v0[0] != v1[0] else np.full((v1[0] - v0[0] + 1), v1[0])
-
-            c_start = v0[0] + alpha * (v2[0] - v0[0])
-            c_end = v1[0] + beta * (v2[0] - v1[0])
-            dc_dx_scan = c_start + (c_end - c_start) * (np.arange(v0[0], v1[0] + 1) - v0[0]) / (
-                v1[0] - v0[0]) if v0[0] != v1[0] else np.full((v1[0] - v0[0] + 1), v1[0])
-
-            # Itera por cada pixel na linha de varredura
-            for x, z, color in zip(range(int(z_start), int(z_end) + 1), dz_dx_scan, dc_dx_scan):
-
-                if x < self.width and y < self.height and x >= 0 and y >= 0:
-                    try:
-                        if z > self.z_buffer[y, x]:
-                            self.z_buffer[y, x] = z
-                            self.screen.set_at(((x, y)), (255, 0, 0))
-                    except Exception as e:
-
-                        print(e)
-                        print(x, y, self.z_buffer.shape)
-                        exit()
-
     def run(self):
         pg.init()
         size = (self.width, self.height)
-        self.screen = pg.display.set_mode(size, display=1, flags=RESIZABLE)
+        self.screen = pg.display.set_mode(size, display=0, flags=RESIZABLE)
         clock = pg.time.Clock()
 
         running = True
@@ -401,7 +446,7 @@ class Cena3D:
                 self.screen, self.camera_lookat[0], self.camera_lookat[1], self.camera_lookat[2])
 
             pg.display.flip()
-            clock.tick(4)  # Limita o loop a 60 frames por segundo
+            clock.tick(24)  # Limita o loop a 60 frames por segundo
 
         pg.quit()
 
